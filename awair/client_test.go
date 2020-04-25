fca9de705eec882faf6253a94c13bc6b5f304999
Nick <penguin@frozendesert.net>
2020-04-25 11:46:05 -0700

    Add tests (#2)

---
+++ awair/client_test.go
@@ -0,0 +1,103 @@
+package awair
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"github.com/arcticfoxnv/awair-exporter/awair/mock"
+	"github.com/arcticfoxnv/awair_api"
+	"github.com/stretchr/testify/assert"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func testingHTTPClient(handler http.Handler) (*http.Client, func()) {
+	s := httptest.NewTLSServer(handler)
+
+	cli := &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, network, _ string) (net.Conn, error) {
+				return net.Dial(network, s.Listener.Addr().String())
+			},
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+
+	return cli, s.Close
+}
+
+func TestClientSetCacheTTL(t *testing.T) {
+	client := NewClient("abc", time.Minute)
+	cache := client.clientCache
+	client.SetCacheTTL(2 * time.Minute)
+	assert.NotEqual(t, cache, client.clientCache)
+}
+
+func TestClientGetDeviceAPIUsage(t *testing.T) {
+	s := mock.NewMockServer()
+	defer s.Close()
+
+	cli := NewClient(mock.ACCESS_TOKEN, time.Minute, awair_api.SetHTTPClient(s.Client()))
+	data, err := cli.GetDeviceAPIUsage("awair-r2", 0)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(data.Usages))
+}
+
+func TestClientGetDeviceList(t *testing.T) {
+	s := mock.NewMockServer()
+	defer s.Close()
+
+	cli := NewClient(mock.ACCESS_TOKEN, time.Minute, awair_api.SetHTTPClient(s.Client()))
+
+	_, found := cli.clientCache.Get(DEVICES_KEY)
+	assert.False(t, found)
+
+	_, err := cli.GetDeviceList()
+	assert.Nil(t, err)
+
+	_, found = cli.clientCache.Get(DEVICES_KEY)
+	assert.True(t, found)
+
+	_, err = cli.GetDeviceList()
+	assert.Nil(t, err)
+}
+
+func TestClientGetLatestData(t *testing.T) {
+	s := mock.NewMockServer()
+	defer s.Close()
+
+	cli := NewClient(mock.ACCESS_TOKEN, time.Minute, awair_api.SetHTTPClient(s.Client()))
+
+	deviceType := "awair-r2"
+	deviceId := 0
+	cacheKey := fmt.Sprintf(DEVICE_LATEST_KEY_FORMAT, deviceType, deviceId)
+	_, found := cli.clientCache.Get(cacheKey)
+	assert.False(t, found)
+
+	_, err := cli.GetLatestData(deviceType, deviceId)
+	assert.Nil(t, err)
+
+	_, found = cli.clientCache.Get(cacheKey)
+	assert.True(t, found)
+
+	_, err = cli.GetLatestData(deviceType, deviceId)
+	assert.Nil(t, err)
+}
+
+func TestClientGetUserInfo(t *testing.T) {
+	s := mock.NewMockServer()
+	defer s.Close()
+
+	cli := NewClient(mock.ACCESS_TOKEN, time.Minute, awair_api.SetHTTPClient(s.Client()))
+	data, err := cli.GetUserInfo()
+
+	assert.Nil(t, err)
+	assert.Equal(t, "Kim", data.LastName)
+	assert.Equal(t, "Steve", data.FirstName)
+}
---
+++ awair/mock/client.go
@@ -0,0 +1,100 @@
+package mock
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const ACCESS_TOKEN = "abc123"
+
+type MockServer struct {
+	Server       *httptest.Server
+	mux          *http.ServeMux
+	testdataPath string
+}
+
+func checkAuthorization(r *http.Request) bool {
+	return r.Header.Get("Authorization") == fmt.Sprintf("Bearer %s", ACCESS_TOKEN)
+}
+
+func getTestDataPath() string {
+	dir, _ := os.Getwd()
+	parts := strings.Split(dir, string(filepath.Separator))
+	i := 0
+	for i = 0; i < len(parts); i++ {
+		if parts[i] == "awair-exporter" {
+			break
+		}
+	}
+
+	return fmt.Sprintf("%c%s", filepath.Separator, filepath.Join(parts[0:i+1]...))
+}
+
+func NewMockServer() *MockServer {
+	m := &MockServer{}
+	m.mux = http.NewServeMux()
+	m.mux.Handle("/v1/users/self/devices", m.checkAuthMiddleware(m.serveFile("Devices.json")))
+	m.mux.Handle("/v1/users/self", m.checkAuthMiddleware(m.serveFile("UserInfo.json")))
+	m.mux.Handle("/v1/users/self/devices/", m.checkAuthMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/awair/0/air-data/latest") {
+			m.serveFile("UserLatestAirData-awair-0.json").ServeHTTP(w, r)
+		} else if strings.HasSuffix(r.URL.Path, "/awair-r2/0/air-data/latest") {
+			m.serveFile("UserLatestAirData-awair-r2-0.json").ServeHTTP(w, r)
+		} else if strings.HasSuffix(r.URL.Path, "/api-usages") {
+			m.serveFile("DeviceAPIUsage.json").ServeHTTP(w, r)
+		} else {
+			w.WriteHeader(404)
+		}
+	})))
+
+	m.testdataPath = filepath.Join(getTestDataPath(), "testdata")
+
+	m.Server = httptest.NewTLSServer(m.mux)
+	return m
+}
+
+func (m *MockServer) Close() {
+	m.Server.Close()
+}
+
+func (m *MockServer) Client() *http.Client {
+	return &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, network, _ string) (net.Conn, error) {
+				return net.Dial(network, m.Server.Listener.Addr().String())
+			},
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+}
+
+func (m *MockServer) checkAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !checkAuthorization(r) {
+			w.WriteHeader(403)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (m *MockServer) serveFile(path string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data, _ := ioutil.ReadFile(filepath.Join(m.testdataPath, path))
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", len(data)))
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write([]byte(data))
+	})
+}
